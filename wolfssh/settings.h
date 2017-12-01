d1a7ff9b16bb48f6b231f0ea39b8c4e235551817
John Safranek <john@wolfssl.com>
2017-12-01 12:03:16 -0800

    Update settings for FreeRTOS

--- wolfssh/settings.h
+++ wolfssh/settings.h
@@ -35,14 +35,80 @@ extern "C" {
 #endif
 
 
+/* Uncomment next line if using FreeRTOS */
+/* #define FREERTOS */
+
+/* Uncomment next line if using FreeRTOS+ TCP */
+/* #define FREERTOS_TCP */
+
+/* Uncomment next line if using FreeRTOS Windows Simulator */
+/* #define FREERTOS_WINSIM */
+
+
 /* user settings here */
 #ifndef WMALLOC_USER
     #define USE_WOLFSSH_MEMORY  /* default memory handlers */
 #endif /* WMALLOC_USER */
 
 
-#if defined (_WIN32)
+#if defined(_WIN32) && !defined(FREERTOS) && !defined(FREERTOS_TCP)
     #define USE_WINDOWS_API
+#endif
+
+
+#ifdef FREERTOS_WINSIM
+    #define FREERTOS
+    #define USE_WINDOWS_API
+#endif
+
+
+#ifdef FREERTOS
+    #include "FreeRTOS.h"
+
+    /* FreeRTOS pvPortRealloc() only in AVR32_UC3 port */
+    #if !defined(XMALLOC_USER) && !defined(NO_WOLFSSL_MEMORY)
+        #define XMALLOC(s, h, type)  pvPortMalloc((s))
+        #define XFREE(p, h, type)    vPortFree((p))
+    #endif
+
+    #ifndef NO_WRITEV
+        #define NO_WRITEV
+    #endif
+    #ifndef HAVE_SHA512
+        #ifndef NO_SHA512
+            #define NO_SHA512
+        #endif
+    #endif
+    #ifndef HAVE_DH
+        #ifndef NO_DH
+            #define NO_DH
+        #endif
+    #endif
+    #ifndef NO_DSA
+        #define NO_DSA
+    #endif
+    #ifndef NO_HC128
+        #define NO_HC128
+    #endif
+
+    #ifndef SINGLE_THREADED
+        #include "semphr.h"
+    #endif
+#endif
+
+#ifdef FREERTOS_TCP
+    #if !defined(NO_WOLFSSL_MEMORY) && !defined(XMALLOC_USER)
+        #define WMALLOC(s, h, type)  pvPortMalloc((s))
+        #define WFREE(p, h, type)    vPortFree((p))
+    #endif
+
+    #define WOLFSSL_GENSEED_FORTEST
+
+    #define NO_WOLFSSL_DIR
+    #define NO_WRITEV
+    #define USE_FAST_MATH
+    #define TFM_TIMING_RESISTANT
+    #define NO_MAIN_DRIVER
 #endif
 
 
